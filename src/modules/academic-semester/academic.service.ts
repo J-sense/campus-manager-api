f41bb860b5a0a8da92d891b2c90640ad9698a2a5
J-sense <jishan1873@gmail.com>
2024-12-02 19:08:52 +0600

    semester code validation

--- src/modules/academic-semester/academic.service.ts
+++ src/modules/academic-semester/academic.service.ts
@@ -2,6 +2,14 @@ import { acadeicSemester } from './academic.interface';
 import { academicModel } from './academic.model';
 
 const createAcademicSemesterIntoDb = async (payload: acadeicSemester) => {
+  const academicSemesterCodeMapper = {
+    Autumn: '01',
+    Summer: '02',
+    Fall: '03',
+  };
+  if (academicSemesterCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Semester code is not valid');
+  }
   const result = await academicModel.create(payload);
   return result;
 };
