2a72a81e368fbfbfc073f4507f99374d2a6a8c57
J-sense <jishan1873@gmail.com>
2024-12-05 08:35:32 +0600

    update validation

--- src/modules/academic-department/academicDepartment.modal.ts
+++ src/modules/academic-department/academicDepartment.modal.ts
@@ -18,13 +18,25 @@ const AcademicDepartmentSchema = new Schema<TacademicDepartment>(
   },
 );
 
-AcademicDepartmentSchema.pre('save', async function () {
+AcademicDepartmentSchema.pre('save', async function (next) {
   const isDepartmentExits = await AcademicDepartment.findOne({
     name: this.name,
   });
   if (isDepartmentExits) {
     throw new Error('This Department is already exist');
   }
+  next();
+});
+
+AcademicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new Error('This department does not exist! ');
+  }
+
+  next();
 });
 export const AcademicDepartment = model<TacademicDepartment>(
   'AcademicDepartment',
