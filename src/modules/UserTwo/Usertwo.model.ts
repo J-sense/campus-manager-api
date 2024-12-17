8fa522ec41a6ef679e9fa4a4b5a7d0b6b5003687
J-sense <jishan1873@gmail.com>
2024-12-17 07:41:57 +0600

    start

---
+++ src/modules/UserTwo/Usertwo.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from 'mongoose';
+import { TUserTwo } from './Usertwo.interface';
+
+const UserSchema = new Schema<TUserTwo>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['student', 'faculty', 'admin'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress,blocked'],
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export const UserTwo = model<TUserTwo>('UserTwo', UserSchema);
